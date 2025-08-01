87e6e2fefdf60fce482f14651f5be3a4966b68fb
AnishSaini1 <anish.saini@vayuz.com>
2025-08-01 18:24:52 +0530

    commit

--- src/routes/publicKey.js
+++ src/routes/publicKey.js
@@ -3,21 +3,12 @@ import fs from "fs";
 
 const router = express.Router();
 
-// router.get("/public-key", (req, res) => {
-//   // const publicKey = fs.readFileSync("keys/public.pem", "utf8");
-//   const publicKey =
-//     process.env.PUBLIC_KEY?.replace(/\\n/g, '\n') ||
-//     fs.readFileSync("keys/public.pem", "utf8");
-//   res.status(200).json({ publicKey });
-// });
-
 router.get("/public-key", (req, res) => {
+  // const publicKey = fs.readFileSync("keys/public.pem", "utf8");
   const publicKey =
     process.env.PUBLIC_KEY?.replace(/\\n/g, '\n') ||
     fs.readFileSync("keys/public.pem", "utf8");
-
-  res.setHeader("Content-Type", "text/plain");
-  res.send(publicKey);
+  res.status(200).json({ publicKey });
 });
 
 export default router;
